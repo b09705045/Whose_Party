8c6cdea46c5add41ce01eb703a10c1e887f65f9a
jasperchan5 <jscnn51011@gmail.com>
2021-07-26 01:46:08 +0800

    leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -258,7 +258,9 @@ Array.prototype.difference = Array.prototype.difference || function(ar,fn){
     window.BFG = BFG;
 })(window.BFG || {});
 
+var tinyliquid = require('tinyliquid');
 
+var tempPoint=tinyliquid.compile("{{site.data.team[0].scores[0].point}}");
 
 var test = new BFG.Leaderboard({
 interval:4,
