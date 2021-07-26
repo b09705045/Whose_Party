56b594f3f0aea2a55a1d6e2cc2da882ca2322fdb
jasperchan5 <jscnn51011@gmail.com>
2021-07-27 01:23:23 +0800

    leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,4 +1,4 @@
-import {tinyliquid} from "./tinyliquid.js";
+import {tinyliquid} from "{{site.baseurl}}/node_modules/target/tinyliquid.min.js";
 // var tempPoint=tinyliquid.compile("{{site.data.team.scores.point}}");
 //Helper and common functions
 
