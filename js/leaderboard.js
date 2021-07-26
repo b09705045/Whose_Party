7b40cd4e80accea22266e403dd02b12c7e7e0d71
jasperchan5 <jscnn51011@gmail.com>
2021-07-26 23:49:45 +0800

    leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,5 +1,5 @@
 //Helper and common functions
-var tinyliquid=require('./tinyliquid.min');
+var tinyliquid=require(tinyliquid.min);
 (function(BFG){
     BFG.extend = function(obj){
         var key,
