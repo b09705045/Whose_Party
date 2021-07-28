ef6229b9a02b6002728d3de5311a7120790c4980
jasperchan5 <jscnn51011@gmail.com>
2021-07-29 01:16:30 +0800

    leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,6 +1,6 @@
 //Helper and common functions
 'use strict';
-var yaml = require("{{./lib/js-yaml.js}}");
+var yaml = require(['./lib/js-yaml.js'],function(jsyaml,$){});
 module.exports = yaml;
 // const jsyaml = require('js-yaml');
 // $.get( '/data/team.yml', function( text ) {
