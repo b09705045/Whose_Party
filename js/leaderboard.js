87caa7048b534b78d30f11a14fdaf4dd45490154
jasperchan5 <jscnn51011@gmail.com>
2021-08-09 00:32:19 +0800

    leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -295,14 +295,14 @@ var test = new BFG.Leaderboard({
 
             return [//simulates incoming data
                 {id: 1,title: "一見到你‧紫想要‧那個",count: scoreList[0][0]},
-                {id: 2,title: "肚子二‧普茲普茲‧布朗尼",count: totalList[1].innerHTML},
-                {id: 3,title: "黑芝麻‧三明治‧嘿咻嘿咻",count: totalList[2].innerHTML},
-                {id: 4,title: "四巴拉稀‧肛鐵人‧歐蘭吉",count: totalList[3].innerHTML},
-                {id: 5,title: "白吃白喝‧爽‧五敵鐵金剛",count: totalList[4].innerHTML},
-                {id: 6,title: "六到開‧瑪莎拉‧蒂芬妮綠",count: totalList[5].innerHTML},
-                {id: 7,title: "藍莓‧七泡水‧史戈史戈",count: totalList[6].innerHTML},
+                {id: 2,title: "肚子二‧普茲普茲‧布朗尼",count: scoreList[1][0]},
+                {id: 3,title: "黑芝麻‧三明治‧嘿咻嘿咻",count: scoreList[2][0]},
+                {id: 4,title: "四巴拉稀‧肛鐵人‧歐蘭吉",count: scoreList[3][0]},
+                {id: 5,title: "白吃白喝‧爽‧五敵鐵金剛",count: scoreList[4][0]},
+                {id: 6,title: "六到開‧瑪莎拉‧蒂芬妮綠",count: scoreList[5][0]},
+                {id: 7,title: "藍莓‧七泡水‧史戈史戈",count: scoreList[6][0]},
                 // {id:8,title:"扒了你‧領主大人‧灰生氣",count:BFG.rnd(1,800)},
-                {id: 8,title: "扒了你‧領主大人‧灰生氣",count: totalList[7].innerHTML},
+                {id: 8,title: "扒了你‧領主大人‧灰生氣",count: scoreList[7][0]},
             ];
         }
 
