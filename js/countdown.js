5ecfbfb49c795fc4bd49235459c9b7ce1e1784d2
jasperchan5 <jscnn51011@gmail.com>
2021-08-16 23:36:38 +0800

    minor fix

--- js/countdown.js
+++ js/countdown.js
@@ -1,35 +1,56 @@
-
-$(document).ready(function() {
-    $('.countdown').final_countdown({
-    start: '1362139200',
-    end: '1388461320',
-    now: '1387461319',
-    selectors: {
-        value_seconds: '.clock-seconds .val',
-        canvas_seconds: 'canvas_seconds',
-        value_minutes: '.clock-minutes .val',
-        canvas_minutes: 'canvas_minutes',
-        value_hours: '.clock-hours .val',
-        canvas_hours: 'canvas_hours',
-        value_days: '.clock-days .val',
-        canvas_days: 'canvas_days'
-    },
-    seconds: {
-        borderColor: '#7995D5',
-        borderWidth: '6'
-    },
-    minutes: {
-        borderColor: '#ACC742',
-        borderWidth: '6'
-    },
-    hours: {
-        borderColor: '#ECEFCB',
-        borderWidth: '6'
-    },
-    days: {
-        borderColor: '#FF9900',
-        borderWidth: '6'
-    }}, function() {
-    // Finish callback
-    });
-});
+const countDownClock = (number = 100, format = 'seconds') => {
+  
+    const d = document;
+    const daysElement = d.querySelector('.days');
+    const hoursElement = d.querySelector('.hours');
+    const minutesElement = d.querySelector('.minutes');
+    const secondsElement = d.querySelector('.seconds');
+    let countdown;
+    convertFormat(format);
+    
+    
+    function convertFormat(format) {
+      switch(format) {
+        case 'seconds':
+          return timer(number);
+        case 'minutes':
+          return timer(number * 60);
+          case 'hours':
+          return timer(number * 60 * 60);
+        case 'days':
+          return timer(number * 60 * 60 * 24);             
+      }
+    }
+  
+    function timer(seconds) {
+      const now = Date.now();
+      const then = now + seconds * 1000;
+  
+      countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+  
+        if(secondsLeft <= 0) {
+          clearInterval(countdown);
+          return;
+        };
+  
+        displayTimeLeft(secondsLeft);
+  
+      },1000);
+    }
+  
+    function displayTimeLeft(seconds) {
+      daysElement.textContent = Math.floor(seconds / 86400);
+      hoursElement.textContent = Math.floor((seconds % 86400) / 3600);
+      minutesElement.textContent = Math.floor((seconds % 86400) % 3600 / 60);
+      secondsElement.textContent = seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
+    }
+  }
+  
+  
+  /*
+    start countdown
+    enter number and format
+    days, hours, minutes or seconds
+  */
+  countDownClock(20, 'days');
