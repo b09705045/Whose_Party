9fa0c2814282645894b2a908351e5c560258e093
jasperchan5 <jscnn51011@gmail.com>
2021-08-17 00:25:08 +0800

    minor fix

--- js/countdown.js
+++ js/countdown.js
@@ -1,56 +1,23 @@
-const countDownClock = (number = 100, format = 'seconds') => {
-  
-    const d = document;
-    const daysElement = d.querySelector('.days');
-    const hoursElement = d.querySelector('.hours');
-    const minutesElement = d.querySelector('.minutes');
-    const secondsElement = d.querySelector('.seconds');
-    let countdown;
-    convertFormat(format);
+var time = new Date();
+        
+time.setMonth(9); 
+time.setDate(23); 
+time.setHours(0); 
+time.setMinutes(0); 
+time.setSeconds(0);
+var endTime = time.getTime();
+
+setInterval(function(){
+    var time_2 = new Date();
+    var nowTime = time_2.getTime(); 
+    var offsetTime = (endTime - nowTime) / 1000; 
+    var sec = parseInt(offsetTime % 60); 
+    var min = parseInt((offsetTime / 60) % 60); 
+    var hr = parseInt((offsetTime / 60 / 60) % 24); 
+    var day = parseInt(offsetTime / 60 / 60 / 24); 
     
-    
-    function convertFormat(format) {
-      switch(format) {
-        case 'seconds':
-          return timer(number);
-        case 'minutes':
-          return timer(number * 60);
-          case 'hours':
-          return timer(number * 60 * 60);
-        case 'days':
-          return timer(number * 60 * 60 * 24);             
-      }
-    }
-  
-    function timer(seconds) {
-      const now = Date.now();
-      const then = now + seconds * 1000;
-  
-      countdown = setInterval(() => {
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
-  
-        if(secondsLeft <= 0) {
-          clearInterval(countdown);
-          return;
-        };
-  
-        displayTimeLeft(secondsLeft);
-  
-      },1000);
-    }
-  
-    function displayTimeLeft(seconds) {
-      daysElement.textContent = Math.floor(seconds / 86400);
-      hoursElement.textContent = Math.floor((seconds % 86400) / 3600);
-      minutesElement.textContent = Math.floor((seconds % 86400) % 3600 / 60);
-      secondsElement.textContent = seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
-    }
-  }
-  
-  
-  /*
-    start countdown
-    enter number and format
-    days, hours, minutes or seconds
-  */
-  countDownClock(20, 'days');
+    document.querySelector('.days').innerHTML = ''+ day +'';
+    document.querySelector('.hours').innerHTML = ''+ hr +'';
+    document.querySelector('.minutes').innerHTML = ''+ min +'';
+    document.querySelector('.seconds').innerHTML = ''+ sec +'';
+},1000);
