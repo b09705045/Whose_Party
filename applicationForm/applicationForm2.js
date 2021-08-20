5dd36fc382d797e177f7d9b22aeca598cf67e98c
Laurence Wang <aalaurence0615@gmail.com>
2021-08-20 16:03:32 +0800

    dataUpdate_v1

--- applicationForm/applicationForm2.js
+++ applicationForm/applicationForm2.js
@@ -191,12 +191,14 @@ submitButtom.addEventListener("click", (e) =>{
   .then(function(){
     console.log("Data saved")
   })
+  .then(function(confirm_submit){
+    this.location.replace("applicationAfConfirm.html")
+    confirm_submit = true;
+    console.log("Change")
+  })
   .catch(function(error){
     console.log(error);
   })
 
-  if(confirm_submit === true){
-    window.location.replace("../../Whose_Party/applicationConfirmed/")
-  }
 })
 
